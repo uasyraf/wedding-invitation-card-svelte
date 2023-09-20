3092bc0eecb0eefb7ae9925b0e1cc3b4b4a67be8
Ummar <ummar.asyraf7@gmail.com>
2023-09-20 12:41:55 +0800

    Initial commit

---
+++ pink-palette-custom-theme.ts
@@ -0,0 +1,103 @@
+
+import type { CustomThemeConfig } from '@skeletonlabs/tw-plugin';
+
+export const pinkPaletteCustomTheme: CustomThemeConfig = {
+    name: 'pink-palette-custom-theme',
+    properties: {
+		// =~= Theme Properties =~=
+		"--theme-font-family-base": `system-ui`,
+		"--theme-font-family-heading": `system-ui`,
+		"--theme-font-color-base": "0 0 0",
+		"--theme-font-color-dark": "255 255 255",
+		"--theme-rounded-base": "9999px",
+		"--theme-rounded-container": "8px",
+		"--theme-border-base": "1px",
+		// =~= Theme On-X Colors =~=
+		"--on-primary": "0 0 0",
+		"--on-secondary": "0 0 0",
+		"--on-tertiary": "0 0 0",
+		"--on-success": "0 0 0",
+		"--on-warning": "0 0 0",
+		"--on-error": "255 255 255",
+		"--on-surface": "255 255 255",
+		// =~= Theme Colors  =~=
+		// primary | #ffc7c0 
+		"--color-primary-50": "255 247 246", // #fff7f6
+		"--color-primary-100": "255 244 242", // #fff4f2
+		"--color-primary-200": "255 241 239", // #fff1ef
+		"--color-primary-300": "255 233 230", // #ffe9e6
+		"--color-primary-400": "255 216 211", // #ffd8d3
+		"--color-primary-500": "255 199 192", // #ffc7c0
+		"--color-primary-600": "230 179 173", // #e6b3ad
+		"--color-primary-700": "191 149 144", // #bf9590
+		"--color-primary-800": "153 119 115", // #997773
+		"--color-primary-900": "125 98 94", // #7d625e
+		// secondary | #ffd7c0 
+		"--color-secondary-50": "255 249 246", // #fff9f6
+		"--color-secondary-100": "255 247 242", // #fff7f2
+		"--color-secondary-200": "255 245 239", // #fff5ef
+		"--color-secondary-300": "255 239 230", // #ffefe6
+		"--color-secondary-400": "255 227 211", // #ffe3d3
+		"--color-secondary-500": "255 215 192", // #ffd7c0
+		"--color-secondary-600": "230 194 173", // #e6c2ad
+		"--color-secondary-700": "191 161 144", // #bfa190
+		"--color-secondary-800": "153 129 115", // #998173
+		"--color-secondary-900": "125 105 94", // #7d695e
+		// tertiary | #fff7c0 
+		"--color-tertiary-50": "255 254 246", // #fffef6
+		"--color-tertiary-100": "255 253 242", // #fffdf2
+		"--color-tertiary-200": "255 253 239", // #fffdef
+		"--color-tertiary-300": "255 252 230", // #fffce6
+		"--color-tertiary-400": "255 249 211", // #fff9d3
+		"--color-tertiary-500": "255 247 192", // #fff7c0
+		"--color-tertiary-600": "230 222 173", // #e6dead
+		"--color-tertiary-700": "191 185 144", // #bfb990
+		"--color-tertiary-800": "153 148 115", // #999473
+		"--color-tertiary-900": "125 121 94", // #7d795e
+		// success | #c7ffc0 
+		"--color-success-50": "247 255 246", // #f7fff6
+		"--color-success-100": "244 255 242", // #f4fff2
+		"--color-success-200": "241 255 239", // #f1ffef
+		"--color-success-300": "233 255 230", // #e9ffe6
+		"--color-success-400": "216 255 211", // #d8ffd3
+		"--color-success-500": "199 255 192", // #c7ffc0
+		"--color-success-600": "179 230 173", // #b3e6ad
+		"--color-success-700": "149 191 144", // #95bf90
+		"--color-success-800": "119 153 115", // #779973
+		"--color-success-900": "98 125 94", // #627d5e
+		// warning | #e7ffc0 
+		"--color-warning-50": "251 255 246", // #fbfff6
+		"--color-warning-100": "250 255 242", // #fafff2
+		"--color-warning-200": "249 255 239", // #f9ffef
+		"--color-warning-300": "245 255 230", // #f5ffe6
+		"--color-warning-400": "238 255 211", // #eeffd3
+		"--color-warning-500": "231 255 192", // #e7ffc0
+		"--color-warning-600": "208 230 173", // #d0e6ad
+		"--color-warning-700": "173 191 144", // #adbf90
+		"--color-warning-800": "139 153 115", // #8b9973
+		"--color-warning-900": "113 125 94", // #717d5e
+		// error | #d7c0ff 
+		"--color-error-50": "249 246 255", // #f9f6ff
+		"--color-error-100": "247 242 255", // #f7f2ff
+		"--color-error-200": "245 239 255", // #f5efff
+		"--color-error-300": "239 230 255", // #efe6ff
+		"--color-error-400": "227 211 255", // #e3d3ff
+		"--color-error-500": "215 192 255", // #d7c0ff
+		"--color-error-600": "194 173 230", // #c2ade6
+		"--color-error-700": "161 144 191", // #a190bf
+		"--color-error-800": "129 115 153", // #817399
+		"--color-error-900": "105 94 125", // #695e7d
+		// surface | #fde8e8 
+		"--color-surface-50": "255 252 252", // #fffcfc
+		"--color-surface-100": "255 250 250", // #fffafa
+		"--color-surface-200": "255 249 249", // #fff9f9
+		"--color-surface-300": "254 246 246", // #fef6f6
+		"--color-surface-400": "254 239 239", // #feefef
+		"--color-surface-500": "253 232 232", // #fde8e8
+		"--color-surface-600": "228 209 209", // #e4d1d1
+		"--color-surface-700": "190 174 174", // #beaeae
+		"--color-surface-800": "152 139 139", // #988b8b
+		"--color-surface-900": "124 114 114", // #7c7272
+		
+	}
+}
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+
+// @ts-check
+import { join } from 'path';
+
+// 1. Import the Skeleton plugin
+import { skeleton } from '@skeletonlabs/tw-plugin';
+
+import { pinkPaletteCustomTheme } from './pink-palette-custom-theme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	// 2. Opt for dark mode to be handled via the class method
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		// 3. Append the path to the Skeleton package
+		join(require.resolve(
+			'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
+	],
+	theme: {
+		extend: {},
+	},
+  plugins: [
+	skeleton({
+		themes: {
+			custom: [
+				pinkPaletteCustomTheme
+			]
+		}
+	})
+  ]  
+}
+						
